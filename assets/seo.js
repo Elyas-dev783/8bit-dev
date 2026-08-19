2c67eee0c405621d6dd67348274d5f7dd8ff21a9
ahmadreza <ahmadrezajafari891@gmail.com>
2026-08-19 21:29:41 +0330

    add functionalyty to project

---
+++ assets/seo.js
@@ -0,0 +1,13 @@
+function greet(user) {
+  return `hi ${user}`;
+}
+
+console.log(greet("Elyas"));
+
+let name = "Zeus";
+
+if (name === "Zeus") {
+  console.log("thats great");
+} else {
+  console.log("Too bad");
+}
